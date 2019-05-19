0817f1ea8a904aea2037a37e7567d9ae9e7cd5fb
Michel Jung <michel.jung89@gmail.com>
2019-05-19 21:59:13 +0200

    Update jacoco to 0.8.4, improve build script

--- ci/test-report.sh
+++ ci/test-report.sh
@@ -2,4 +2,4 @@
 
 # TODO re-enable TLSv1.3
 # See https://github.com/kt3k/coveralls-gradle-plugin/issues/85 and https://bugs.openjdk.java.net/browse/JDK-8221253
-./gradlew -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" jacocoTestReport coveralls
+./gradlew -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" jacocoTestReport coveralls sendCoverageToCodacy

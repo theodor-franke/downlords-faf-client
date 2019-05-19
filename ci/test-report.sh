a00672730947d1fcfec182d40a7c42cf4f603d0d
Michel Jung <michel.jung89@gmail.com>
2019-05-19 21:08:49 +0200

    Workaround for JDK-8221253

--- ci/test-report.sh
+++ ci/test-report.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew jacocoTestReport coveralls
+# TODO re-enable TLSv1.3
+# See https://github.com/kt3k/coveralls-gradle-plugin/issues/85 and https://bugs.openjdk.java.net/browse/JDK-8221253
+./gradlew -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" jacocoTestReport coveralls

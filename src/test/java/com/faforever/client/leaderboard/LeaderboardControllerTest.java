e939774d858152a6669245d49d30c621846a7c47
balla <s6krball@uni-bonn.de>
2020-07-16 20:00:45 +0200

    adapted tests

--- src/test/java/com/faforever/client/leaderboard/LeaderboardControllerTest.java
+++ src/test/java/com/faforever/client/leaderboard/LeaderboardControllerTest.java
@@ -62,87 +62,8 @@ public class LeaderboardControllerTest extends AbstractPlainJavaFxTest {
   }
 
   @Test
-  public void testOnDisplay() throws Exception {
-    when(leaderboardService.getEntries(KnownFeaturedMod.LADDER_1V1)).thenReturn(CompletableFuture.completedFuture(Arrays.asList(
-        new LeaderboardEntry(), new LeaderboardEntry()
-    )));
-
-    CountDownLatch loadedLatch = new CountDownLatch(1);
-    instance.ratingTable.itemsProperty().addListener(observable -> loadedLatch.countDown());
-    instance.setRatingType(KnownFeaturedMod.LADDER_1V1);
-
-    instance.display(new OpenLadder1v1LeaderboardEvent());
-
-    assertTrue(loadedLatch.await(3, TimeUnit.SECONDS));
-    verifyZeroInteractions(notificationService);
-  }
-
-  @Test
-  public void testFilterByNamePlayerExactMatch() {
-    LeaderboardEntry entry1 = new LeaderboardEntry();
-    entry1.setUsername("Aa");
-    LeaderboardEntry entry2 = new LeaderboardEntry();
-    entry2.setUsername("Ab");
-
-    when(leaderboardService.getEntries(KnownFeaturedMod.LADDER_1V1)).thenReturn(CompletableFuture.completedFuture(Arrays.asList(
-        entry1, entry2
-    )));
-    instance.setRatingType(KnownFeaturedMod.LADDER_1V1);
-    instance.display(new OpenLadder1v1LeaderboardEvent());
-
-    assertThat(instance.ratingTable.getSelectionModel().getSelectedItem(), nullValue());
-
-    instance.searchTextField.setText("aa");
-    assertThat(instance.ratingTable.getItems(), hasSize(2));
-    assertThat(instance.ratingTable.getSelectionModel().getSelectedItem().getUsername(), is("Aa"));
-  }
-
-  @Test
-  public void testFilterByNamePlayerPartialMatch() {
-    LeaderboardEntry entry1 = new LeaderboardEntry();
-    entry1.setUsername("Aa");
-    LeaderboardEntry entry2 = new LeaderboardEntry();
-    entry2.setUsername("Ab");
-
-    when(leaderboardService.getEntries(KnownFeaturedMod.LADDER_1V1)).thenReturn(CompletableFuture.completedFuture(Arrays.asList(
-        entry1, entry2
-    )));
-    instance.setRatingType(KnownFeaturedMod.LADDER_1V1);
-    instance.display(new OpenLadder1v1LeaderboardEvent());
-
-    assertThat(instance.ratingTable.getSelectionModel().getSelectedItem(), nullValue());
-
-    instance.searchTextField.setText("b");
-    assertThat(instance.ratingTable.getItems(), hasSize(2));
-    assertThat(instance.ratingTable.getSelectionModel().getSelectedItem().getUsername(), is("Ab"));
-  }
-
-  @Test
   public void testGetRoot() throws Exception {
     assertThat(instance.getRoot(), is(instance.leaderboardRoot));
     assertThat(instance.getRoot().getParent(), is(nullValue()));
-  }
-
-  @Test
-  public void testOnContextMenuRequested() {
-    ContextMenu contextMenu = mock(ContextMenu.class);
-    ContextMenuEvent contextMenuEvent = mock(ContextMenuEvent.class);
-    LeaderboardEntry leaderboardEntry = new LeaderboardEntry();
-
-    when(userContextMenuController.getContextMenu()).thenReturn(contextMenu);
-    when(uiService.loadFxml("theme\\leaderboard\\leaderboard_user_context_menu.fxml")).thenReturn(userContextMenuController);
-
-    WaitForAsyncUtils.asyncFx(() -> getRoot().getChildren().setAll(instance.leaderboardRoot));
-
-    Player player = PlayerBuilder.create("mockito").defaultValues().get();
-    userContextMenuController.setPlayer(player);
-    //when(instance.selectedEntryList.get(anyInt())).thenReturn(leaderboardEntry);
-    when(leaderboardService.getPlayerObjectsById(any(String.class))).thenReturn(CompletableFuture.completedFuture(Arrays.asList(player)));
-
-    WaitForAsyncUtils.waitForFxEvents();
-
-    instance.onContextMenuRequested(contextMenuEvent);
-
-    verify(contextMenu).show(any(Window.class), anyDouble(), anyDouble());
   }
 }

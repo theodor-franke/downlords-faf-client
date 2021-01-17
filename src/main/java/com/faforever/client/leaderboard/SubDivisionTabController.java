f2e90916f408397254ef61ee4bc9600e13bf145f
BlackYps <jonas-fb@gmx.de>
2021-01-17 17:06:52 +0100

    Add winloss column

--- src/main/java/com/faforever/client/leaderboard/SubDivisionTabController.java
+++ src/main/java/com/faforever/client/leaderboard/SubDivisionTabController.java
@@ -4,36 +4,33 @@ import com.faforever.client.fx.Controller;
 import com.faforever.client.fx.StringCell;
 import com.faforever.client.i18n.I18n;
 import com.faforever.client.notification.NotificationService;
-import com.faforever.client.reporting.ReportingService;
+import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.lang.invoke.MethodHandles;
-
 import static javafx.collections.FXCollections.observableList;
 
+
+@Slf4j
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @RequiredArgsConstructor
 public class SubDivisionTabController implements Controller<Tab> {
 
-  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private final LeaderboardService leaderboardService;
   private final NotificationService notificationService;
   private final I18n i18n;
-  private final ReportingService reportingService;
-
   public Tab subDivisionTab;
   public TableColumn<LeagueEntry, Number> rankColumn;
   public TableColumn<LeagueEntry, String> nameColumn;
+  public TableColumn<LeaderboardEntry, Number> winLossColumn;
   public TableColumn<LeagueEntry, Number> gamesPlayedColumn;
   public TableColumn<LeagueEntry, Number> scoreColumn;
   public TableView<LeagueEntry> ratingTable;
@@ -51,6 +48,9 @@ public class SubDivisionTabController implements Controller<Tab> {
     nameColumn.setCellValueFactory(param -> param.getValue().usernameProperty());
     nameColumn.setCellFactory(param -> new StringCell<>(name -> name));
 
+    winLossColumn.setCellValueFactory(param -> new SimpleFloatProperty(param.getValue().getWinLossRatio()));
+    winLossColumn.setCellFactory(param -> new StringCell<>(number -> i18n.get("percentage", number.floatValue() * 100)));
+
     gamesPlayedColumn.setCellValueFactory(param -> param.getValue().gamesPlayedProperty());
     gamesPlayedColumn.setCellFactory(param -> new StringCell<>(count -> i18n.number(count.intValue())));
 
@@ -61,10 +61,10 @@ public class SubDivisionTabController implements Controller<Tab> {
   public void populate(Division division) {
     subDivisionTab.setText(i18n.get(division.getSubDivisionName().getI18nKey()).toUpperCase());
 
-    leaderboardService.getEntries(division).thenAccept(leagueEntryBeans -> {
-      ratingTable.setItems(observableList(leagueEntryBeans));
-    }).exceptionally(throwable -> {
-      logger.warn("Error while loading leaderboard entries", throwable);
+    leaderboardService.getEntries(division).thenAccept(leagueEntryBeans ->
+        ratingTable.setItems(observableList(leagueEntryBeans)))
+        .exceptionally(throwable -> {
+      log.warn("Error while loading leaderboard entries for division " + division.toString(), throwable);
       notificationService.addImmediateErrorNotification(throwable, "leaderboard.failedToLoad");
       return null;
     });
